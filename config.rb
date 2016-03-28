9750460c900ffd2bc952c56953b70dfa517308f7
Mateus <mjabourbp@gmail.com>
2016-03-28 17:02:56 -0400

    Changing header title font family

--- config.rb
+++ config.rb
@@ -1,13 +1,7 @@
-preferred_syntax = :sass
-http_path = '/'
-css_dir = 'assets/css/dev'
+http_path = "/"
+css_dir = 'assets/css'
 sass_dir = 'assets/sass'
 images_dir = 'assets/img'
-relative_assets = true
 
-output_style = :expanded
-line_comments = true
-
-on_stylesheet_saved do
-  `compass compile -c config_dev.rb --force`
-end
+output_style = :compressed
+line_comments = false
--- config_dev.rb
+++ config_dev.rb
@@ -1,7 +1,13 @@
-http_path = "/"
-css_dir = 'assets/css'
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'assets/css/dev'
 sass_dir = 'assets/sass'
 images_dir = 'assets/img'
+relative_assets = true
 
-output_style = :compressed
-line_comments = false
+output_style = :expanded
+line_comments = true
+
+on_stylesheet_saved do
+  `compass compile -c config.rb --force`
+end
